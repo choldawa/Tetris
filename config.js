2b373533043123eb40861fa5a099acb684ce4f72
Cameron Holdaway <cameron.holdaway@gmail.com>
2020-06-23 21:58:13 -0700

    aesthetics

--- config.js
+++ config.js
@@ -1,38 +1,78 @@
-// board dimensions and scaling
-const cols = 10;
-const rows = 20;
-const block_size = 30;
+'use strict';
 
-// block colors and shapes
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+const LINES_PER_LEVEL = 10;
+const COLORS = [
+  'none',
+  '#3891A6',
+  '#EAC435',
+  '#0C7C59',
+  '#A155B9',
+  '#D64933',
+  '#17BEBB',
+  '#EDB88B',
+  '#255957'
+];
+Object.freeze(COLORS);
 
-// points
+const SHAPES = [
+  [],
+  [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]], //pipe
+  [[2, 0, 0], [2, 2, 2], [0, 0, 0]], //L
+  [[0, 0, 3], [3, 3, 3], [0, 0, 0]], //J
+  [[4, 4], [4, 4]], //square
+  [[0, 5, 5], [5, 5, 0], [0, 0, 0]], // S
+  [[0, 6, 0], [6, 6, 6], [0, 0, 0]], //T
+  [[7, 7, 0], [0, 7, 7], [0, 0, 0]] // Z
+];
+Object.freeze(SHAPES);
 
-// levels
-
-// pressed keys
 const KEY = {
-    LEFT: 37,
-    RIGHT: 39,
-    DOWN: 40
-  }
+  ESC: 27,
+  SPACE: 32,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  P: 80
+}
+Object.freeze(KEY);
 
-  document.addEventListener('keydown', event => {
-    if (moves[event.keyCode]) {  
-      // Stop the event from bubbling.
-      event.preventDefault();
-      
-      // Get new state of piece
-      let p = moves[event.keyCode](board.block);
-      
-      if (board.valid(p)) {    
-        // If the move is valid, move the piece.
-        board.block.move(p);
-        
-        // Clear old position before drawing.
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); 
-        
-        board.block.draw();
-      }
-    }
-  })
+const POINTS = {
+  SINGLE: 100,
+  DOUBLE: 300,
+  TRIPLE: 500,
+  TETRIS: 800,
+  SOFT_DROP: 1,
+  HARD_DROP: 2,
+}
+Object.freeze(POINTS);
+
+const LEVEL = {
+  0: 800,
+  1: 720,
+  2: 630,
+  3: 550,
+  4: 470,
+  5: 380,
+  6: 300,
+  7: 220,
+  8: 130,
+  9: 100,
+  10: 80,
+  11: 80,
+  12: 80,
+  13: 70,
+  14: 70,
+  15: 70,
+  16: 50,
+  17: 50,
+  18: 50,
+  19: 30,
+  20: 30,
+  // 29+ is 20ms
+}
+Object.freeze(LEVEL);
 
